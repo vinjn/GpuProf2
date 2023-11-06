380f8f23a9ad6d1dcd060457a6c97a7f9fe66c9d
Jing Zhang <vinjn@users.noreply.github.com>
2023-11-05 20:45:39 -0800

    Initial commit

---
+++ update_deps.sh
@@ -0,0 +1,17 @@
+cp ../sokol/sokol_*.h sokol/
+cp ../sokol/util/sokol_imgui.h sokol/
+
+cp ../fips-cimgui/cimgui/cimgui.cpp cimgui/
+cp ../fips-cimgui/cimgui/cimgui.h cimgui/
+
+cp ../fips-cimgui/cimgui/imgui/imconfig.h cimgui/imgui
+cp ../fips-cimgui/cimgui/imgui/imgui.cpp cimgui/imgui
+cp ../fips-cimgui/cimgui/imgui/imgui.h cimgui/imgui
+cp ../fips-cimgui/cimgui/imgui/imgui_internal.h cimgui/imgui
+cp ../fips-cimgui/cimgui/imgui/imgui_demo.cpp cimgui/imgui
+cp ../fips-cimgui/cimgui/imgui/imgui_draw.cpp cimgui/imgui
+cp ../fips-cimgui/cimgui/imgui/imgui_widgets.cpp cimgui/imgui
+cp ../fips-cimgui/cimgui/imgui/imgui_tables.cpp cimgui/imgui
+cp ../fips-cimgui/cimgui/imgui/imstb_rectpack.h cimgui/imgui
+cp ../fips-cimgui/cimgui/imgui/imstb_textedit.h cimgui/imgui
+cp ../fips-cimgui/cimgui/imgui/imstb_truetype.h cimgui/imgui
